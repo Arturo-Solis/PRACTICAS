2d7321b58592038a002b99f353179acf5fa3015a
Arturo-Solis <solisbarron031119@gmail.com>
2025-03-29 16:49:54 -0600

    Update practica9.php

--- practica9.php
+++ practica9.php
@@ -7,36 +7,40 @@
 </head>
 <body><center>  
     <header>
+        //titulo
         <h1><mark>  Dado un numero, devolver el doble del numero si el numero es positivo,
              el triple si es negativo, cero si es neutro </mark></h1>
     </header>
     <main>
         <section>
-            <form method="POST">
+            <form method="POST"> //verifica el metodo POST
                 <label for="num1">Ingresa un numero: </label><br>
                 <input type="number" id="num1" name="num1"><br><br>
-                <button type="submit">Enviar</button><br>
+                <button type="submit">Enviar</button><br> //boton que envia los datos
             </form>
     <?php
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         if(isset($_POST["num1"])){
+            //variable
             $numero=$_POST["num1"];
 
-            if($numero>0){
-                echo"Este numero es positivo."."\n\n";
-                echo"El resultado al multiplicarlo por dos es: ".$numero*2;
-        }else if($numero<0){
-            echo "Ese numero es negativo $numero\n\n";
-            echo "El resultado al multiplicarlo por 3 es: ".$numero*3;
+            if($numero>0){ //verifica que sea mayor a cero
+                echo"Este numero es positivo."."\n\n"; //imprime el mensaje
+                echo"El resultado al multiplicarlo por dos es: ".$numero*2; //imprime el mensaje y multiplica por dos el numero
+        }else if($numero<0){ //verifica que sea menor a cero
+            echo "Ese numero es negativo $numero\n\n"; //imprime el mensaje
+            echo "El resultado al multiplicarlo por 3 es: ".$numero*3; //imprime el mensaje y multiplica por tres el numero
         }else{
-            echo "Este numero es neutro";
+            echo "Este numero es neutro"; //imprime el mensaje
         }
     }
 }
     ?><br><br><br>
-    <a href="practica10.php">Practica 1</a>
+    //link a otra practica
+    <a href="practica10.php">Practica 10</a>
         </section>
     </main>
+    //pie de pagina
     <footer>Arturo Solis</footer>
     </center>
 </body>
