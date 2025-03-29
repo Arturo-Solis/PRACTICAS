d54483d78f7de9297b3c98d441b7eb97e6f4955e
Arturo-Solis <solisbarron031119@gmail.com>
2025-03-29 15:28:52 -0600

    Update practica4.php

--- practica4.php
+++ practica4.php
@@ -8,31 +8,38 @@
 <body>
     <center>  
     <header>
+        //titulo
         <h1><mark>Determina si un numero es par o impar </mark></h1>
     </header>
     <main>
         <section>
 
-    <form method="POST">
+    <form method="POST"> //verifica el metodo POST
+        //formulario que interactua con el usuario
         <label for="numero">Ingresa un numero: </label><br>
         <input type="number" id="numero" name="numero"><br><br>
-        <button type="submit">Enviar</button><br>
+        <button type="submit">Enviar</button><br> //boton que envia el formulario
     </form>
+    //bloque de php
     <?php
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         if(isset($_POST["numero"])){
+            //variable
             $num=$_POST['numero'];
-            if($num%2==0){
-                echo"El $num es un numero par.";
+            
+            if($num%2==0){ //verifica que el nunero ingresado dividido entre 2 de cero, lo cual sera par
+                echo"El $num es un numero par."; //imprime el mensaje
         }else{
-            echo "El $num es un numero impar.";
+            echo "El $num es un numero impar."; //imprime el mensaje
     }
 }
 }
     ?><br><br><br>
+    //link a otra pagina
     <a href="practica5.php">Practica 5</a>
     </section>
         </main>
+        //pie de pagina
         <footer>Arturo Solis</footer>
     </center>
 </body>
