3b927a263f3509996a114972f99cd70ef4bff233
Arturo-Solis <solisbarron031119@gmail.com>
2025-03-29 15:32:57 -0600

    Update practica5.php

--- practica5.php
+++ practica5.php
@@ -7,32 +7,38 @@
 </head>
 <body><center>  
     <header>
+        //titulo
         <h1><mark>dado un numero, devolver el doble si el numero no es par, caso contrario devolver el triple </mark></h1>
     </header>
     <main>
         <section>
-            <form method="POST">
+            <form method="POST"> //verifica el metodo POST
                 <label for="numero">Ingresa un numero: </label><br>
                 <input type="number" id="numero" name="numero" required><br>
-                <button type="submit">Enviar</button>
+                <button type="submit">Enviar</button> //boton que envia los datos
             </form>
-            
+            //bloque de php
             <?php
             if($_SERVER["REQUEST_METHOD"]=="POST"){
+                //variable
                 $num=$_POST['numero'];
-                if($num%2== 0){
-                    echo"El numero es par <br>";
-                    echo"El resultado es ".($num*2);
+                
+                if($num%2== 0){ //verifica que el numero dividido entre 2 de cero, entonces es par
+                    echo"El numero es par <br>"; //imprime el mensaje
+                    echo"El resultado es ".($num*2); //ya dicho que el numero es par ahora se multiplica por 2
                 }else{
-                    echo "El numero es impar<br>";
-                    echo "El resultado es".($num*3);
+                    echo "El numero es impar<br>"; //imprime el mensaje
+                    echo "El resultado es".($num*3); //ya verificado que es impar, se multiplica por 3
 
                 }
             }
             ?><br><br><br>
+                //link a otra practica
 <a href="practica6.php">Practica 6</a>
         </section>
     </main>
+
+    //pie de pagina
     <footer>Arturo Solis</footer>
     </center>
 </body>
