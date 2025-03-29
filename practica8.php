e367ef0abda7d033e523ea288e222bd0e3c5ad8a
Arturo-Solis <solisbarron031119@gmail.com>
2025-03-29 16:39:34 -0600

    Update practica8.php

--- practica8.php
+++ practica8.php
@@ -8,30 +8,36 @@
 <body>
     <center> 
     <header>
+        //titulo
         <h1><mark>Dado la edad de una persona determinar si es mayor de edad o menor de edad  </mark></h1>
     </header>
     <main>
         <section>
-            <form method="POST">
+            <form method="POST"> //verifica el motodo POST
                 <label for="edade">Ingrese su edad</label><br>
                 <input type="number" id="edade" name="edade"><br><br>
-                <button type="submit">Enviar</button>
+                <button type="submit">Enviar</button> // boton envia los datos
             </form>
+            //bloque de php
             <?php
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         if(isset($_POST["edade"])){
-            $edad=$_POST['edade'];
-            if($edad>=18 ){
-                echo"Eres mayor de edad ";
+            //variable
+            $edad=$_POST['edade']; 
+            
+            if($edad>=18 ){ //verifica si 'edad' es mayor o igual a 18 
+                echo"Eres mayor de edad "; //imprime el mensaje
         }else {
-            echo "Eres menor de edad";
+            echo "Eres menor de edad"; //imprime el mensaje si no
         }
     }  
 }
             ?><br><br><br>
+//link para otra practica
 <a href="practica9.php">Practica9</a>
         </section>
     </main>
+    //pie de pagina
     <footer>Arturo solis</footer>
     </center>
 </body>
