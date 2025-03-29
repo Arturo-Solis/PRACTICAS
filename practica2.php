dbe74d746981c30e22c9487f2d4348669f9d5255
Arturo-Solis <solisbarron031119@gmail.com>
2025-03-29 15:19:21 -0600

    practica2.php

--- practica2.php
+++ practica2.php
@@ -7,39 +7,44 @@
 </head>
 <body>
     <center>  
-    <header>
+    <header> //titulo
     <mark><h1>Dado un caracter determina si es una vocal</h1> </mark>
     </header>
     <main>
         <section>
 
-    <form method="POST">
+    <form method="POST"> //verifica el metodo POST
+        //formulario que interactua con el usuario
         <label for="letra">Ingresa un caracter</label>
         <input type="text" id="letra" name="letra">
-        <button type="submit">Enviar</button>
+        <button type="submit">Enviar</button> //boton para enviar los datos 
     </form>
+    //bloque de php
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(isset($_POST["letra"])){
+        //variable
         $letra=$_POST['letra'];
-        if($letra=='a'|| $letra=='A'){
-            echo"Esta letra es una vocal";
-        }else if($letra=='e'||$letra=='E'){
-            echo "Esta letra es una vocal";
-        }else if($letra=='i'||$letra=='I'){
-            echo "Esta letra es una vocal";
-        }else if($letra=='o'||$letra=='O'){
-            echo "Esta letra es una vocal";
-        }else if($letra=='u'||$letra=='U'){
-    echo "Esta letra es una vocal";
+        if($letra=='a'|| $letra=='A'){ //verifica si es 'a' o 'A'
+            echo"Esta letra es una vocal"; //imprime el mensaje 
+        }else if($letra=='e'||$letra=='E'){ //verifica si es 'e' o 'E'
+            echo "Esta letra es una vocal"; //imprime el mensaje 
+        }else if($letra=='i'||$letra=='I'){ //verifica si es 'i' o 'I'
+            echo "Esta letra es una vocal"; //imprime el mensaje 
+        }else if($letra=='o'||$letra=='O'){ //verifica si es 'o' o 'O'
+            echo "Esta letra es una vocal"; //imprime el mensaje 
+        }else if($letra=='u'||$letra=='U'){ //verifica si es 'u' o 'U'
+    echo "Esta letra es una vocal"; //imprime el mensaje 
 }else{
-    echo "Esta letra no es una vocal";
+    echo "Esta letra no es una vocal"; //imprime el mensaje 
 }
 }}
-?><br><br><br>
+?><br><br><br> 
+//link a otra practica 
 <a href="practica3.php">Practica 3</a>
 </section>
-        </main>
+        </main> 
+        //pie de pagina
         <footer>Arturo Solis</footer>
 </center>
 </body>
