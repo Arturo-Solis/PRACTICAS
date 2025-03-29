ad6ac62bca2e14eb3a77607d98598f675ee8b637
Arturo-Solis <solisbarron031119@gmail.com>
2025-03-29 16:22:49 -0600

    Update practica7.php

--- practica7.php
+++ practica7.php
@@ -8,35 +8,41 @@
 <body>
     <center>
         <header>
+            //titulo
             <h1><mark>Elabore un algoritmo que resuelva una ecuacion de primer grado </mark></h1>
         </header>
         <main>
             <section>
-                <form method="POST">
+                <form method="POST"> //verifica el metodo POST
+                    //formulario que interactua con el usuario
                     <label for="num1">Ingresa un valor "a": </label><br>
                     <input type="number" id="num1" name="num1"><br>
                     <label for="num2">Ingrese un valor "b"</label><br>
                     <input type="number" id="num2" name="num2"><br>
-                    <button type="submmit">Enviar</button>
+                    <button type="submmit">Enviar</button> //boton que envia los datos
                 </form>
+    //bloque de php
     <?php
     if($_SERVER["REQUEST_METHOD"]=="POST"){
     if (isset($_POST['num1']) && isset($_POST['num2'])) {
+        //variables
         $a=$_POST['num1'];
         $b=$_POST['num2'];
 
-        if($a != 0){
-            $x = (-$b / $a);
-            echo"El resultado de x es: $x";
+        if($a != 0){ //verifica que 'a' sea diferente de cero 
+            $x = (-$b / $a); //calcula el valor de x mediante la ecuacion
+            echo"El resultado de x es: $x"; //imprime el mensaje y resultado 
         }else{
-            echo"a no se puede dividir entre 0";
+            echo"a no se puede dividir entre 0"; //imprime el mensaje 
         }
     }
     }
     ?><br><br><br>
+//link a otra practica
 <a href="practica8.php">Practica 8</a>
             </section>
         </main>
+        //pie de pagina
         <footer>Arturo solis</footer>
     </center>
 </body>
